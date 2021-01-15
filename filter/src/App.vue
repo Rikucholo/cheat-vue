d9234033c9e0baf12bfc4009af3f5e3a98b8f5c9
RikuCholo <starcloud.nal.aircraft@gmail.com>
2021-01-15 22:05:36 +0900

    feat(client):フィルタのサンプルコードを書いた

---
+++ filter/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+<FilterFilter/>
+  </div>
+</template>
+
+<script>
+import FilterFilter from './components/FilterFilter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FilterFilter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

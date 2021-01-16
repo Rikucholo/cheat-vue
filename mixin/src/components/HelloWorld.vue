2824808e6f93770204cf244a36f26bfc2ac712b5
RikuCholo <starcloud.nal.aircraft@gmail.com>
2021-01-16 22:56:26 +0900

    WIP:filter example

---
+++ mixin/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+ <div id="HelloWorld">
+  <h2>{{ title }}</h2>
+  <h3>{{ upper }}</h3>
+  <h3>{{ sub | toupper }}</h3>
+  <h3>{{ main | toupper }}</h3>
+  <h3>{{ low | lower }}</h3>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  
+  data(){
+    return {
+      title: "hello world",
+      sub: "sub hello",
+      main: "sukmkb hello",
+      low: "SDRGS",
+    };
+  },
+
+  computed:{
+    upper(){
+      return this.title.toUpperCase();
+    }
+  },
+
+  filters:{
+    lower(value){
+      return value.toLowerCase();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ mixin/src/components/NewCom.vue
@@ -0,0 +1,41 @@
+<template>
+ <div id="NewCom">
+  <h2>{{ title }}</h2>
+  <h3>{{ upper }}</h3>
+  <h3>{{ sub | toupper }}</h3>
+  <h3>{{ main | toupper }}</h3>
+  <h3>{{ low | lower }}</h3>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'NewCom',
+  
+  data(){
+    return {
+      title: "hello world",
+      sub: "sub hello",
+      main: "sukmkb hello",
+      low: "SDRGS",
+    };
+  },
+
+  computed:{
+    upper(){
+      return this.title.toUpperCase();
+    }
+  },
+
+  filters:{
+    lower(value){
+      return value.toLowerCase();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

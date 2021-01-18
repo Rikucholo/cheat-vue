85413dc4670fb4f911175a41a8fd6e53b9dc459c
RikuCholo <starcloud.nal.aircraft@gmail.com>
2021-01-18 22:06:28 +0900

    feat(client):feed in 実装

---
+++ feedout/src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <transition name="fade">
+      <div v-if="show">これが消えていく</div>
+    </transition>
+    <transition name="fade2">
+      <div v-if="show">これが消えていく</div>
+      <div v-else>これが現れる</div>
+    </transition>
+    <transition-group>
+      <div v-for="item in items" :key="item">
+        <p>{{ item }}</p>
+      </div>
+    </transition-group>
+    <button @click="show = !show">button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      show: true,
+      items: ["appple", "banana", "orange", "grep"],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.fade-enter {
+  /* 現れる時の最初の状態 */
+  opacity: 0;
+}
+.fade-enter-active {
+  /* 現れるときのトランザクションの状態 */
+  transition: opacity 5s;
+}
+.fade-enter-to {
+  /* 現れるときの最後の状態 */
+  opacity: 1;
+}
+.fade-leave {
+  /* 消えるときけい */
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: opacity 5s;
+  animation: slide-in 0.5s reverse;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+@keyframes slide-in {
+  from {
+    transform: translateX(100px);
+  }
+  to {
+    transform: translateX(0px);
+  }
+}
+.fade2-enter {
+}
+.fade2-enter-active {
+  animation: slide-in 0.5s;
+}
+.fade2-enter-to {
+}
+.fade2-leave {
+}
+.fade2-leave-active {
+}
+.fade2-leave-to {
+}
+</style>

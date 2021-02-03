e79893c60d74053a5daf0b6e8fb4407671726100
RikuCholo <starcloud.nal.aircraft@gmail.com>
2021-02-03 22:30:42 +0900

    feat:post実装

--- http-axios/src/App.vue
+++ http-axios/src/App.vue
@@ -2,14 +2,50 @@
   <div id="app">
     <h3>投稿する</h3>
     <label for="name">ニックネーム：</label>
-    <input type="text" id="name" />
+    <input type="text" id="name" v-model="name" />
     <br />
     <label for="comment">コメント:</label>
-    <textarea id="comment"></textarea>
+    <textarea id="comment" v-model="comment"></textarea>
+    <button @click="post">POST</button>
+    <h2>掲示板</h2>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      name: "",
+      comment: "",
+    };
+  },
+  methods: {
+    post() {
+      axios
+        .post(
+          "https://firestore.googleapis.com/v1/projects/vue-http-c0133/databases/(default)/documents/comments",
+          {
+            fields: {
+              name: {
+                stringValue: this.name,
+              },
+              comment: {
+                stringValue: this.comment,
+              },
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+      [this.name, this.comment] = "";
+    },
+  },
+};
 </script>
 
 <style>
